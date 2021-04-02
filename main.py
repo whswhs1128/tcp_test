4015289169f37e8321a998a00e64b68b672ae8f8
wanghs <whswhs1128@gmail.com>
2021-04-02 10:44:20 +0800

    whs:添加接受buffer

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ server = socket.socket()
 
 
 def start_tcp_server():
-    server.bind(('172.16.16.168', 50001)) #server ip
-    # server.bind(('127.0.0.1', 50001)) #test ip
+    # server.bind(('172.16.16.168', 50001)) #server ip
+    server.bind(('127.0.0.1', 50001))  # test ip
     server.listen()
 
 
@@ -27,22 +27,42 @@ if __name__ == '__main__':
         start_tcp_server()
     except:
         pass
-    key = bytearray(100) #帧长度
-    key[0] = 0xaa  # 帧头
-    key[1] = 0x55  # 帧头
-    key[14] = 0x1  # 帧类型 1：参数设置 2：状态数据
-    key[15] = 0x1  # 帧方向 1：控制器到显示屏 2：显示屏到控制器
-    key[22] = 0x1  # 实例数据
-    key[23] = 0x1  # 实例数据
-    print_hex(key)
+    setting = bytearray(800)  # 帧长度
+    setting[0] = 0xaa  # 帧头
+    setting[1] = 0x55  # 帧头
+    setting[2] = 3  # 帧头
+    setting[3] = 32  # 帧头
+    setting[14] = 0x1  # 帧类型 1：参数设置 2：状态数据
+    setting[15] = 0x1  # 帧方向 1：控制器到显示屏 2：显示屏到控制器
+    setting[22] = 0x1  # 实例数据
+    setting[23] = 0x1  # 实例数据
+    # print_hex(key)
+    state = bytearray(800)  # 帧长度
+    state[0] = 0xaa  # 帧头
+    state[1] = 0x55  # 帧头
+    state[2] = 3  # 帧头
+    state[3] = 32  # 帧头
+    state[14] = 0x2  # 帧类型 1：参数设置 2：状态数据
+    state[15] = 0x1  # 帧方向 1：控制器到显示屏 2：显示屏到控制器
+    state[22] = 0x1  # 实例数据
+    state[23] = 0x1  # 实例数据
+    recv_buff = bytearray(800)
 
     while True:
         conn, addr = server.accept()
         while True:
-            print_hex(key)
-            if not key:
-                print("Don't has data")
+            conn.send(setting)
+            # print_hex(setting)
+            conn.send(state)
+            # print_hex(state)
+            # if not key:
+            #     print("Don't has data")
+            #     break
+            time.sleep(1)
+            recv_buff = conn.recv(800)
+            if not recv_buff:
+                print_hex(recv_buff)
                 break
-            time.sleep(2)
-            conn.send(key)
+
+
     server.close()
